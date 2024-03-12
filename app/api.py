60d35f718363c4c50a8e384f43cb7e3bb1d771a3
luis-borges <kdegrize@gmail.com>
2024-03-12 11:07:17 +0100

    paho mqtt launch

--- app/api.py
+++ app/api.py
@@ -2,6 +2,7 @@ from typing import Union
 from fastapi import FastAPI
 from models.Controller import Controller
 from app.service import *
+from app.mqtt_paho import *
 
 app = FastAPI()
 
@@ -9,11 +10,20 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+def transcript(etat: bool):
+    if etat:
+        return 1
+    else:
+        return 0
+
 @app.get("/")
 async def read_root():
     return {"chauffage": "Controleur API "}
 
 @app.post("/etat_chauffage/{etat}")
 async def update_etat_chauffage(etat: bool):
-    envoieFireBase(etat)
+    #envoieFireBase(etat)
+    client.publish("CONTROL", payload=transcript(etat))
+    client.loop_start()
     return {"etatChauffagee : ": etat}
+
