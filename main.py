d6774fca73fa86713daa85f24d608a7bb31aaa06
luis-borges <kdegrize@gmail.com>
2024-05-17 14:21:10 +0200

    add log to mqtt3

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from dotenv import load_dotenv
 import os 
 import uvicorn
+from app.mqtt_paho import *
+import threading
 
 load_dotenv()
 PORT = int(os.get('PORT', 8000))
 HOST = '0.0.0.0'
+
 if __name__ == '__main__':
+    # Start MQTT client in a separate thread
+    mqtt_thread = threading.Thread(target=startMQTT)
+    mqtt_thread.start()
+
+    # Start Uvicorn server
     uvicorn.run('app.api:app', host=HOST, port=PORT, reload=True)
