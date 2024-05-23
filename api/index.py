b183de7dffae529ee804583dcb88e78ca68a6c3f
luis-borges <kdegrize@gmail.com>
2024-05-23 15:58:24 +0200

    add cron with Mqtt with time20

--- api/index.py
+++ api/index.py
@@ -39,10 +39,10 @@ async def update_etat_chauffage(etat: bool):
 async def test_cron():
     client.loop_stop()
     # Run for 60 seconds
-    end_time = asyncio.get_event_loop().time() + 50
+    end_time = asyncio.get_event_loop().time() + 40
     while asyncio.get_event_loop().time() < end_time:
         client.loop_start()
-        await asyncio.sleep(0.87)  # Sleep for 1 second
+        await asyncio.sleep(40//60)  # Sleep for 1 second
 
     return {"CRON": " every 5 minutes "}
 
@@ -50,11 +50,11 @@ async def test_cron():
 @app.post("/cron")
 async def test_cron_post():
     client.loop_stop()
-    # Run for 60 seconds
-    end_time = asyncio.get_event_loop().time() + 50
+    # Run for 40 seconds
+    end_time = asyncio.get_event_loop().time() + 40
     while asyncio.get_event_loop().time() < end_time:
         client.loop_start()
-        await asyncio.sleep(87)  # Sleep for 1 second
+        await asyncio.sleep(40//60)  # Sleep for this second
 
 
     return {"CRON": "every 1 minute"}
