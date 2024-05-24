0ce461f97e07f37ac34245a49b475fabd53cfb4d
luis-borges <kdegrize@gmail.com>
2024-05-24 11:35:29 +0200

    add routine for getting production_energy add prediction test 5

--- api/index.py
+++ api/index.py
@@ -64,9 +64,9 @@ async def test_cron_post():
 # Le cron est géré par notre service QStash en ligne
 @app.post("/cron_get_energie_produite_from_chauffage")
 async def cron_get_energie_produite_from_chauffage():
-    client.loop_write()
+    client.loop_stop()
     client.publish(ENERGY_ASK, payload=1)
-    client.loop_write()
+    client.loop_start()
     # end_time = asyncio.get_event_loop().time() + TIME_OUT-10  # 10 seconde pour le temps du publish
     # while asyncio.get_event_loop().time() < end_time:
     #     client.loop_read()
@@ -80,9 +80,9 @@ async def cron_get_energie_produite_from_chauffage():
 # Le cron est géré par notre service QStash en ligne
 @app.post("/cron_send_daily_prediction_to_app")
 async def cron_send_daily_prediction_to_app():
-    client.loop_write()
+    client.loop_stop()
     client.publish(PREDICTION, payload="{'matin': 16, 'midi': 18, 'soir': 15}")
-    client.loop_write()
+    client.loop_start()
 
     return {"QStash CRON Daily prediction": "every 23h"}
 
